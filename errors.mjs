d6273e6f4ff2b7ea0cd7ea5250afb56f63b705d4
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-25 19:18:11 +0100

    process.on('uncaughtException)

--- errors.mjs
+++ errors.mjs
@@ -1,6 +1,10 @@
 import {readFile} from 'fs/promises'
 
-readFile(new URL('app.js', import.meta.url), 'utf-8').catch(e=> {
-    console.error(e);
-    console.log('heyo');
+process.on('uncaughtException', (e) => {
+    console.log(e)
+    // unhandled exception got caught and this is the last synchronous block you can execute before it shuts down
+    // this is not handling the error, app will shutdown after this tick
 })
+
+const result = await readFile(new URL('app.js', import.meta.url), 'utf-8')
+console.log('hello')

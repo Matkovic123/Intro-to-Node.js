3ede6a3224c791bb2ae2903f1e1502139533ee98
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-25 19:12:43 +0100

    Error handling promises

--- errors.mjs
+++ errors.mjs
@@ -1,8 +1,6 @@
-import {readFile} from 'fs'
+import {readFile} from 'fs/promises'
 
-try {
-const result = await readFile(new URL('app.js', import.meta.url), 'utf-8')
-} catch(e) {
+readFile(new URL('app.js', import.meta.url), 'utf-8').catch(e=> {
     console.error(e);
     console.log('heyo');
-}
+})

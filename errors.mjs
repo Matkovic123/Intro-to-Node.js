0aa8c1191e2e9cec21005806382507290191c031
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-25 19:11:29 +0100

    Handling errors with async await

--- errors.mjs
+++ errors.mjs
@@ -1,10 +1,8 @@
 import {readFile} from 'fs'
 
-readFile(new URL('app.js', import.meta.url), 'utf-8', (err, data) => {
-    if(err) {
-        console.error("handling ma shit hier") // handles gracefully
-        throw err; // terminates process
-    } else {
-
-    }
-})
+try {
+const result = await readFile(new URL('app.js', import.meta.url), 'utf-8')
+} catch(e) {
+    console.error(e);
+    console.log('heyo');
+}
